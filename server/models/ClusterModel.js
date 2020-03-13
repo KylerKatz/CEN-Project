5cd90c73fbc818edc0bb39fef46f9eb4dcb8202d
steathbomer <36576479+steathbomer@users.noreply.github.com>
2020-03-13 18:38:22 -0500

    Added DB population script
    Populates atlas from the clusters.json file via JSONToMongo.js

---
+++ server/models/ClusterModel.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+/* Create your schema for the data in the listings.json file that will define how data is saved in your database
+     See https://mongoosejs.com/docs/guide.html for examples for creating schemas
+     See also https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
+  */
+const clusterSchema = new mongoose.Schema({
+  
+  name: {type: String, required: true},
+  id: {type: mongoose.Number, required: true},
+  jobnum: {type: String, required: true},
+  careers: [mongoose.Schema.Types.Mixed]
+  //careers: { type:array, [job: { "id": {type: mongoose.Number, required: true}, "name": {type: String, required: true}, "salary": {type: mongoose.Number, required: true}}]}
+  
+});
+
+/* Use your schema to instantiate a Mongoose model
+Export the model to make it avaiable to other parts of your Node application */
+//Check out - https://mongoosejs.com/docs/guide.html#models
+export default mongoose.model('cluster', clusterSchema);
--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
+import config from './config/config.js';
 const express = require('./config/express.js')
  
 // Use env port or default
 const port = process.env.PORT || 5000;
+
+mongoose.connect(config.db.uri, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}).then(() => {
+    console.log(`Successfully connected to mongoose database.`)
+});
 
 const app = express.init()
 app.listen(port, () => console.log(`Server now running on port ${port}!`));
