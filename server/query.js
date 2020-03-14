e050ce7b5ab03ac7ef7d8e45d2d6a5fbd3e26121
steathbomer <36576479+steathbomer@users.noreply.github.com>
2020-03-13 19:03:21 -0500

    Create query.js

---
+++ server/query.js
@@ -0,0 +1,26 @@
+import * as fs from 'fs';
+import mongoose from 'mongoose';
+import cluster from './models/ClusterModel.js';
+import config from './config/config.js';
+
+let db;
+db= mongoose.connect(config.db.uri, {useNewUrlParser: true});
+mongoose.set('useFindAndModify', true);
+mongoose.set('useCreateIndex', true);
+console.log(`established connection to db at uri: ${config.db.uri}!`);
+
+const retrieveAllClusters = () => {
+ 
+    cluster.find({}, function (err, clusters) {
+		if(err) throw err;
+		
+		var iloveiterators=clusters.length;
+		for(var i = 0; i < iloveiterators; i++){
+			console.log(clusters[i]);
+			
+		};
+		
+	});
+};
+
+retrieveAllClusters();
