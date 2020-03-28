a78505efc5d58d5a5dd03f78d59dbd4f40dcb987
Noah <36576479+steathbomer@users.noreply.github.com>
2020-03-27 19:10:06 -0500

    fixed redirect

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,11 +1,12 @@
 import User from '../models/UserSchema.js'; 
 import mongoose from 'mongoose';
 import path from 'path';
+import express from 'express';
 
-const create = function(req, res){
+export const create = function(req, res, next){
 
     //validate fields 
-
+    
     var addUser = new 
     User({
         name: req.body.name, 
@@ -19,18 +20,38 @@ const create = function(req, res){
         if (err) throw err; 
         //console.log("user created");
         //res.send(data);
-        console.log(path.join(path.resolve(), './client/build/Explore.html'));
-        if(!req.body.isAdmin){
-            res.redirect('/Explore');
-		}
-        else{
-            res.redirect('/Admin-Dashboard');
-		}
+        
     }); 
+    console.log(req.body.isAdmin);
+    next();
+    
 
+    
 
 };
 
+export const redir = function(req, res){
+    console.log("Redirect: ", req.body.isAdmin);
+    console.log(req.body);
+    var t=req.body.isAdmin;
+    //User.find({email})
+    if(t==='false'){
+     console.log('student');
+	}
+    else{
+     console.log('teacher');
+	}
+
+    if(t==='false'){
+         res.redirect('/Explore');
+         
+	}
+    else{
+         res.redirect('/Admin-Dashboard');
+         
+	}
+
+}
 //const check = 
 
-export default create;
+//export default userController;
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,8 +1,8 @@
-import create from '../controllers/usersController.js';
+import * as userController from '../controllers/usersController.js';
 import express from 'express';
 const userRouter = express.Router();
 
 //var userController 
-userRouter.post("/", create);
+userRouter.post("/", userController.create, userController.redir);
 
 export default userRouter; 
