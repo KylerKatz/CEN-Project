fcec8bf0930838c8b0462cd2783d5358fd25a50b
Noah <36576479+steathbomer@users.noreply.github.com>
2020-04-15 18:04:48 -0500

    Added CRUD routes

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -6,5 +6,8 @@ const userRouter = express.Router();
 
 //var userController 
 userRouter.post("/", userController.validate, userController.create, userController.redir);
+userRouter.put("/Password/", userController.update);
+userRouter.put("/Teacher/", userController.assign);
+userRouter.put("/Delete/", userController.delete);
 
 export default userRouter; 
