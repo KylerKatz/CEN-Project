c2af068b4b6d90d2782610b64dcaa04ea977547a
KylerKatz <57510356+KylerKatz@users.noreply.github.com>
2020-04-24 13:27:28 -1000

    ClusterCard add fix

--- client/src/views/Explore/components/ClusterCard.js
+++ client/src/views/Explore/components/ClusterCard.js
@@ -3,43 +3,49 @@ import { NavLink } from 'react-router-dom';
 import CareerPage from '../../Career-Pages/CareerPage';
 import Explore from '../Explore';
 
-function addCluster() {}
+function addCluster() {
+	console.log('hello');
+	alert('You have been logged out');
+}
 
 //import Background from './../ClusterCardPictures/Marketing.png'
 class ClusterCard extends React.Component {
 	render() {
 		return (
-			<NavLink
-				className="career-links"
-				onClick={() => window.scrollTo(0, 0)}
-				to={{
-					pathname: '/CareerPage/',
-					//component: CareerPage,
-					state: {
-						careers: this.props.cluster.careers,
-					},
-				}}
-			>
-				<div className="career-card">
-					<div
-						className="cluster-image-container"
-						//style={{ backgroundImage: `url(${Background})` }}
-					>
-						<img
-							className="add-icon-on-card"
-							src={'./plus-sign.png'}
-							alt="add"
-							title="Add Cluster"
-							onClick={() => {
-								// this.addCluster();
-							}}
-						/>
-						<span className="cluster-card-text">
-							{this.props.cluster.name}{' '}
-						</span>
+			<div className="career-card-main">
+				<img
+					className="add-icon-on-card"
+					src={'./plus-sign.png'}
+					alt="add"
+					title="Add Cluster"
+					onClick={() => {
+						addCluster();
+					}}
+				/>
+
+				<NavLink
+					className="career-links"
+					onClick={() => window.scrollTo(0, 0)}
+					to={{
+						pathname: '/CareerPage/',
+						//component: CareerPage,
+						state: {
+							careers: this.props.cluster.careers,
+						},
+					}}
+				>
+					<div className="career-card">
+						<div
+							className="cluster-image-container"
+							//style={{ backgroundImage: `url(${Background})` }}
+						>
+							<span className="cluster-card-text">
+								{this.props.cluster.name}{' '}
+							</span>
+						</div>
 					</div>
-				</div>
-			</NavLink>
+				</NavLink>
+			</div>
 		);
 	}
 }
